bba6d75c86f97ac126c069edd9ce9a0e6a0b1584
soul <shaojy@hua-cloud.com.cn>
2022-07-13 11:23:51 +0800

    update

---
+++ android/src/main/java/com/soul/rn/multibundle/constant/RNException.java
@@ -0,0 +1,27 @@
+package com.soul.rn.multibundle.constant;
+
+public class RNException {
+    public int code;
+    public String errorMessage;
+
+    public RNException(int code, String errorMessage) {
+        this.code = code;
+        this.errorMessage = errorMessage;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+}
--- android/src/main/java/com/soul/rn/multibundle/iface/Callback.java
+++ android/src/main/java/com/soul/rn/multibundle/iface/Callback.java
@@ -1,6 +1,9 @@
 package com.soul.rn.multibundle.iface;
 
+import com.soul.rn.multibundle.constant.RNException;
+
 public interface Callback {
     void onSuccess(Object result);
     void onError(String errorMsg);
+    default void onError(RNException exception) {};
 }
